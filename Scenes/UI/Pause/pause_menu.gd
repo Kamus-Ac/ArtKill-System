6ee3ad261e2b090045b0304d35910bb2527dd5cf
mikE446web <ghostmike464@gmail.com>
2026-05-01 19:29:53 -0600

    Menu de pausa
    -Ya pausa completamente todo
    -Ya no castea la ulti en Pausa

--- Scenes/UI/Pause/pause_menu.gd
+++ Scenes/UI/Pause/pause_menu.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var level_base = $"../../.."
 
 func _on_menu_principal_pressed() -> void:
-	Engine.time_scale = 1
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/UI/Main_Menu/main_menu.tscn")
 	
 func _on_resumen_pressed() -> void:
