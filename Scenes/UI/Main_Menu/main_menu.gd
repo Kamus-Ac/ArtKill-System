4ed493832e71a0e21d0d3db5cbfee647ccf6e8b8
razanda055-web <razanda055@gmail.com>
2026-05-03 00:10:46 -0600

    Revert "Mainmenu_control"
    This reverts commit fcd81873855b6361043b9fb3ab86cf8b968b3af4.

--- Scenes/UI/Main_Menu/main_menu.gd
+++ Scenes/UI/Main_Menu/main_menu.gd
@@ -1,28 +1,24 @@
 extends Control
 
-# Las rutas están perfectas, coinciden con tu VBoxContainer
-@onready var yugar: TextureButton = $Fondo/VBoxContainer/YUGAR
-@onready var ajustes: TextureButton = $Fondo/VBoxContainer/AJUSTES
-@onready var zalir: TextureButton = $Fondo/VBoxContainer/ZALIR
 
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# 1. Aseguramos que el ratón sea visible
+	# Aseguramos que el ratón sea visible si vienes de un juego en 3D/FPS
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
-	# 2. EL ARREGLO ESTÁ AQUÍ:
-	# Tienes que decirle a Godot *quién* recibe el foco. 
-	# Al poner "yugar.grab_focus()", el mando ya sabe dónde empezar.
-	if yugar:
-		yugar.grab_focus()
 
-func _process(_delta: float) -> void:
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	pass
+
 
 func _on_yugar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/character_select.tscn")
 
+
 func _on_zalir_pressed() -> void:
 	get_tree().quit()
+
 
 func _on_ajustes_pressed() -> void:
 	pass # Replace with function body.
