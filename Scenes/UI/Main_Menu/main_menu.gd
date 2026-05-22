224d81e6ba2c0441d329b0efecc5ef11ca3ae686
razanda055-web <razanda055@gmail.com>
2026-05-22 02:51:42 -0600

    Otra vez, main menu chido
    Namas me falta cambiar el borde de los botones focused, ya checare eso

--- Scenes/UI/Main_Menu/main_menu.gd
+++ Scenes/UI/Main_Menu/main_menu.gd
@@ -1,23 +1,25 @@
 extends Control
 
 # Referencias a tus botones dentro del VBoxContainer
-@onready var yugar: TextureButton = $Fondo/VBoxContainer/YUGAR
-@onready var ajustes: TextureButton = $Fondo/VBoxContainer/AJUSTES
-@onready var zalir: TextureButton = $Fondo/VBoxContainer/ZALIR
+@onready var jugar: Button = $VBoxContainer2/JUGAR
+@onready var ajustes: Button = $VBoxContainer2/AJUSTES
+@onready var salir: Button = $VBoxContainer2/SALIR
 
 func _ready() -> void:
 	#Aseguramos que el ratón sea visible
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 	# Intentamos dar el foco al arrancar
-	if yugar:
-		yugar.grab_focus()
-
-func _on_yugar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/UI/character_select.tscn")
-
-func _on_zalir_pressed() -> void:
-	get_tree().quit()
+	if jugar:
+		jugar.grab_focus()
 
 func _on_ajustes_pressed() -> void:
 	pass # Aquí irá tu lógica de ajustes
+
+
+func _on_jugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/UI/character_select.tscn")
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
