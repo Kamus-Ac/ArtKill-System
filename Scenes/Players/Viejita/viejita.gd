15fc7eddc26b802f5b56c038843474769bc3d470
Dessmond665 <dessmond665@gmail.com>
2026-05-23 09:42:57 -0600

    doña tomi death animation

--- Scenes/Players/Viejita/viejita.gd
+++ Scenes/Players/Viejita/viejita.gd
@@ -37,23 +37,23 @@ var ulti_radius := 100.0
 var ulti_speed := 8.0
 
 func start(p: CharacterBody2D):
-    player = p
-    ulti_center = player.global_position
-    ulti_angle = 0.0
-    GameManager.ult_tries += 1
+	player = p
+	ulti_center = player.global_position
+	ulti_angle = 0.0
+	GameManager.ult_tries += 1
 
 func ulti_move(delta: float) -> void:
-    if player == null:
-        return
+	if player == null:
+		return
 
-    ulti_angle += delta * ulti_speed
+	ulti_angle += delta * ulti_speed
 
-    # Posición objetivo en el círculo
-    var target_pos = ulti_center + Vector2(
-        cos(ulti_angle),
-        sin(ulti_angle)
-    ) * ulti_radius
+	# Posición objetivo en el círculo
+	var target_pos = ulti_center + Vector2(
+		cos(ulti_angle),
+		sin(ulti_angle)
+	) * ulti_radius
 
-    # Convertir diferencia de posición en velocidad
-    player.velocity = (target_pos - player.global_position) / delta
-    player.move_and_slide()
+	# Convertir diferencia de posición en velocidad
+	player.velocity = (target_pos - player.global_position) / delta
+	player.move_and_slide()
