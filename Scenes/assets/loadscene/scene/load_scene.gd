8adeac8d4f3ad13a411f171f395aa9efcd0132f1
Contraley <miltongmz2001@hotmail.com>
2026-05-23 04:10:42 -0600

    sfx main menu
    si

--- Scenes/assets/loadscene/scene/load_scene.gd
+++ Scenes/assets/loadscene/scene/load_scene.gd
@@ -1,12 +1,15 @@
 extends Camera2D
 
 @onready var loadscene_animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var loading_music = $LoadingMusic
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	loadscene_animation.play("loadscene2")
+	loading_music.play()
 
 
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
+	loading_music.stop()
 	get_tree().change_scene_to_file("res://Scenes/Levels/level_base.tscn")
